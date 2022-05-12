08ccc963f846ee8e9c1d360cd2ef0bdb5a6976bd
Mariano Carbajo <104107401+MarianoCarbajo@users.noreply.github.com>
2022-05-12 10:04:04 -0300

    Corrección de errores Se le dio valor a la variable de tipo string Se agregaron signos de "=" faltantes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 let contador: number = 1;
-let clave: string;
+let clave: string = "";
 
-while (contador <= 3 && clave != "eureka") {
+while (contador <= 3 && clave !== "eureka") {
   contador = contador + 1;
   clave = prompt("Ingrese la clave");
 }
-if (clave == "eureka") {
+if (clave === "eureka") {
   console.log("Clave correcta");
 } else {
   console.log("Agoto la cantidad de intentos");
